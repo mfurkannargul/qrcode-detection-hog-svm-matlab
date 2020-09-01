49057948a051c09fa0d7106f8aead550f1bd8bfb
mfurkannargul <furkannargul98@gmail.com>
2020-09-01 15:31:31 +0300

    Dataset optimized
    The dataset that gives maximum accuracy (91%) has been chosen.

--- testSVMClassifier.m
+++ testSVMClassifier.m
@@ -33,41 +33,5 @@ for i = start:numberTestImages
     end
 end
 
+%% PREDICTION ACCURACY
 accuracy = correctPrediction / (numberTestImages - start + 1) * 100
-
-%% INITIALIZE VARIABLES FOR SLIDING DETECTION WINDOW
-stepSize = 5;
-width = 100;
-height = 40;
-[y x] = size(imageTest)
-position = zeros(y,x);
-y = floor((y-height)/stepSize)+1;
-x = floor((x-width)/stepSize)+1;
-
-%% IMPLEMENT DETECTION WINDOW
-y1 = 1;
-x1 = 1;
-for i = 1:y
-    for j = 1:x
-        image = imageTest(x1:x1+39,y1:y1+99);
-        image = imresize(image,[128 64]);
-        [featureVector,hogVisualization] = extractHOGFeatures(image,'CellSize',[2 2]);
-        [prediction,scores] = predict(SVMModel,featureVector);
-        if (string(prediction) == 'positive')
-            pause(1);
-            position(x1,y1) = -scores(2);
-        end
-        y1 = y1 + stepSize
-    end
-    y1 = 1;
-    x1 = x1 + stepSize
-end
-
-%% DISPLAY DETECTION IN BOUNDING BOX
-[y1 x1] = find(position > 0.75);
-figure(6);
-imshow(imageTest)
-for i = 1:length(y1)
-    pause(1);
-    imrect(gca, [x1(i),y1(i),100,40])
-end
